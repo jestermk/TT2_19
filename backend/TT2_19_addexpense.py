44967567e5fbc1c79be87651d4068b2fa1548702
Malcolm Kee <malcolm_kee@hotmail.com>
2021-11-11 15:25:05 +0800

    Added link to mysql

--- backend/TT2_19_addexpense.py
+++ backend/TT2_19_addexpense.py
@@ -5,6 +5,20 @@ from api import db
 from datetime import datetime
 
 
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'project_expenses'
+ 
+mysql = MySQL(app)
+cursor = mysql.connection.cursor()
+
 class AddExpense(Form):
     id_field = HiddenField()
     project_id = 0 #project instance
@@ -24,3 +38,19 @@ class AddExpense(Form):
     updated_at = DateTimeField(datetime.utcnow)
     updated_by = 0 #user instance
     send = SubmitField('send')
+
+
+@app.route('/login', methods = ['POST', 'GET'])
+def add_expense():
+    if request.method == 'GET':
+        return "Add new expense record"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['discretion']
+        age = request.form['armor']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
