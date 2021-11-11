bdf5f6ba159812f3650748f8aed57b1871fa9bda
jerlyn99 <jjerlynlim@gmail.com>
2021-11-11 11:41:51 +0800

    signin page

--- frontend/budgetapp19/src/App.js
+++ frontend/budgetapp19/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import Auth from './components/auth/auth'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" exact element={<Auth/>}/>
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
+
