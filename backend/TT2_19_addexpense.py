ecc1dc0f01a2bc2f2b464ea4eae9295df5ae9d67
Malcolm Kee <malcolm_kee@hotmail.com>
2021-11-11 13:45:18 +0800

    Debug/minor change to fields

--- backend/TT2_19_addexpense.py
+++ backend/TT2_19_addexpense.py
@@ -1,25 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, FloatField, SubmitField
+from wtforms import Form, StringField, SelectField, FloatField, SubmitField, HiddenField, DateTimeField
+from wtforms.validators import InputRequired, Length
 from api import db
 from datetime import datetime
 
 
-class AddExpense(FlaskForm):
+class AddExpense(Form):
     id_field = HiddenField()
-    project_id = #take from form origin
+    project_id = 0 #project instance
     category_id = SelectField('Choose category', [InputRequired()], 
         choices = [('production', 'Production'), ('operating', 'Operating'), 
         ('financial', 'Financial'), ('vendor', 'Vendor'), ('manpower', 'Manpower'),
         ('software, Software'), ('hardware, Hardware')])
     name = StringField('Expense name', [InputRequired(),
         Regexp(r'^[A-Za-z\s\-\'\/]+$', message="Invalid name"), Length(max=50, 
-        message="Name too long"])
+        message="Name too long")])
     description = StringField('Expense name', [InputRequired(),
         Regexp(r'^[A-Za-z\s\-\'\/]+$', message="Invalid description"), Length(max=50, 
-        message="Description too long"])
+        message="Description too long")])
     amount = FloatField('Expended amount', [ InputRequired() ])
-    created_at = datetime.utcnow
-    created_by = #take from user
-    updated_at = datetime.utcnow
-    updated_by = #take from user
+    created_at = DateTimeField(datetime.utcnow)
+    created_by = 0 #user instance
+    updated_at = DateTimeField(datetime.utcnow)
+    updated_by = 0 #user instance
     send = SubmitField('send')
