ee7a85e3804df070118cce55fa037835cc59fa5e
jerlyn99 <jjerlynlim@gmail.com>
2021-11-11 11:51:25 +0800

    signin basic logic

--- frontend/budgetapp19/src/App.js
+++ frontend/budgetapp19/src/App.js
@@ -1,5 +1,6 @@
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
 import Auth from './components/auth/auth'
+import Home from './components/home/home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 const App = () => {
   
@@ -8,6 +9,7 @@ const App = () => {
       <Container maxWidth="lg">
         <Routes>
           <Route path="/" exact element={<Auth/>}/>
+          <Route path="/home" exact element={<Home/>}/>
         </Routes>
       </Container>
     </BrowserRouter>
---
+++ frontend/budgetapp19/src/components/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h2>WELCOME TO BUDGET APP by group 19 :D</h2>
+        </div>
+    )
+}
+
+export default Home
